736dd22a33dcb34e5eb5d3b4d939939dea1dc324
porama <porama@porama.dev>
2024-01-08 00:40:20 +0700

    add c++ runner image

--- build_images.sh
+++ build_images.sh
@@ -1,2 +1,3 @@
 docker build -f dockerfile/runner_go/Dockerfile -t rin_go --progress plain .
 docker build -f dockerfile/runner_c/Dockerfile -t rin_c --progress plain .
+docker build -f dockerfile/runner_cpp/Dockerfile -t rin_cpp --progress plain .
